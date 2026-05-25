7f4101f70cd6688d53b58b19e4f1ba8d166e9cb2
vtspaid <vtspaid@gmail.com>
2026-05-25 13:37:26 -0400

    working version with app.R file for rconnect

--- tests/testthat/test-fct_helpers.R
+++ tests/testthat/test-fct_helpers.R
@@ -8,27 +8,13 @@ test_that("convert_coords works", {
                                 "Long",
                                 "Lat",
                                 "EPSG:4326",
-                                "EPSG:26917")
+                                "EPSG:26917",
+                                "x",
+                                "y",
+                                0)
 
   expect_equal(round(test_result$x, 1),
-               c(641082.5, 642300.6, 640672.8, 641060.2))
+               c(641082, 642301, 640673, 641060))
 
   expect_equal(round(test_result$y), c(4405648, 4339055, 4427848, 4406858))
-})
-
-test_that("add_coords works", {
-  test_wb <- openxlsx2::wb_load(data_path)
-  test_wb$from_x_name <- "Long"
-  test_wb$from_y_name <- "Lat"
-  test_wb$from_crs <- "EPSG:4326"
-  test_wb$to_crs <- "EPSG:26917"
-
-  expect_no_error(test_wb$add_coords())
-
-  test_result <- openxlsx2::wb_data(test_wb)
-
-  expect_equal(round(test_result$x, 1),
-               c(642300.6, 555935.9, 623597.0, 606504.6))
-
-  expect_equal(round(test_result$y), c(4339055, 4337961, 4427551, 4428503))
 })
